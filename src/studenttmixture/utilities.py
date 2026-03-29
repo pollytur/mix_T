52c7fdbf662908afc6c413ac604b2a5565f8fb14
Polina Turishcheva <pturishcheva@gmail.com>
2026-03-29 21:20:40 +0200

    swicth back to loop for cpu

--- src/studenttmixture/utilities.py
+++ src/studenttmixture/utilities.py
@@ -22,12 +22,34 @@ def sq_maha_distance(X, loc_, scale, covariance_type='full', xp=None):
     """
     if xp is None:
         xp = np
+
+    use_gpu = xp is not np
+
+    if use_gpu:
+        return _sq_maha_distance_gpu(X, loc_, scale, covariance_type, xp)
+    return _sq_maha_distance_cpu(X, loc_, scale, covariance_type)
+
+
+def _sq_maha_distance_cpu(X, loc_, scale, covariance_type):
+    """CPU path: loop over K components with scipy triangular solve."""
+    sq_maha_dist = np.empty((X.shape[0], loc_.shape[0]))
+    for i in range(loc_.shape[0]):
+        diff = X - loc_[None, i, :]
+        if covariance_type == 'diag':
+            sq_maha_dist[:, i] = ((diff**2) / scale[:, i][np.newaxis, :]).sum(axis=1)
+        else:
+            diff = scipy.linalg.solve_triangular(
+                    scale[:, :, i], diff.T, lower=True)
+            sq_maha_dist[:, i] = (diff**2).sum(axis=0)
+    return sq_maha_dist
+
+
+def _sq_maha_distance_gpu(X, loc_, scale, covariance_type, xp):
+    """GPU path: vectorized operations to minimize kernel launches."""
     if covariance_type == 'diag':
-        # Vectorized: broadcast (N,1,M) - (1,K,M) -> (N,K,M), then sum
         diff = X[:, xp.newaxis, :] - loc_[xp.newaxis, :, :]
         sq_maha_dist = xp.sum(diff**2 / scale.T[xp.newaxis, :, :], axis=2)
     else:
-        # Batched solve: L is (K,M,M), diff is (K,N,M)
         L = xp.transpose(scale, (2, 0, 1))
         diff = X[xp.newaxis, :, :] - loc_[:, xp.newaxis, :]
         solved = xp.linalg.solve(L, diff.transpose(0, 2, 1))
@@ -40,12 +62,51 @@ def scale_update_calcs(X, ru, loc_, resp_sum, reg_covar, covariance_type='full',
     step for EM and as the parameter update for variational methods."""
     if xp is None:
         xp = np
+
+    use_gpu = xp is not np
+
+    if use_gpu:
+        return _scale_update_calcs_gpu(X, ru, loc_, resp_sum, reg_covar,
+                                       covariance_type, xp)
+    return _scale_update_calcs_cpu(X, ru, loc_, resp_sum, reg_covar,
+                                   covariance_type)
+
+
+def _scale_update_calcs_cpu(X, ru, loc_, resp_sum, reg_covar, covariance_type):
+    """CPU path: loop over K components with direct BLAS calls."""
     M = loc_.shape[1]
     K = loc_.shape[0]
+
+    if covariance_type == 'diag':
+        scale_ = np.empty((M, K))
+        scale_decomp = np.empty((M, K))
+    else:
+        scale_ = np.empty((M, M, K))
+        scale_decomp = np.empty((M, M, K))
+
+    for i in range(K):
+        diff = X - loc_[i:i+1, :]
+        with np.errstate(under='ignore'):
+            if covariance_type == 'diag':
+                diag_vals = np.sum(ru[:, i:i+1] * diff**2, axis=0) \
+                        / (resp_sum[i] + 10 * np.finfo(scale_.dtype).eps)
+                diag_vals += reg_covar
+                scale_[:, i] = diag_vals
+                scale_decomp[:, i] = diag_vals
+            else:
+                scale_[:, :, i] = np.dot(ru[:, i] * diff.T, diff) \
+                        / (resp_sum[i] + 10 * np.finfo(scale_.dtype).eps)
+                scale_[:, :, i].flat[::M+1] += reg_covar
+                scale_decomp[:, :, i] = np.linalg.cholesky(scale_[:, :, i])
+    return scale_, scale_decomp
+
+
+def _scale_update_calcs_gpu(X, ru, loc_, resp_sum, reg_covar, covariance_type, xp):
+    """GPU path: vectorized operations to minimize kernel launches."""
+    M = loc_.shape[1]
     eps = 10 * np.finfo(np.float64).eps
 
     if covariance_type == 'diag':
-        # Vectorized diagonal scale update
         diff = X[xp.newaxis, :, :] - loc_[:, xp.newaxis, :]  # (K, N, M)
         scale_ = xp.sum(ru.T[:, :, xp.newaxis] * diff**2, axis=1)  # (K, M)
         scale_ /= (resp_sum[:, xp.newaxis] + eps)
@@ -53,7 +114,6 @@ def scale_update_calcs(X, ru, loc_, resp_sum, reg_covar, covariance_type='full',
         scale_ = scale_.T  # (M, K) for API compat
         scale_decomp = scale_.copy()
     else:
-        # Vectorized full scale update using einsum + batched Cholesky
         diff = X[xp.newaxis, :, :] - loc_[:, xp.newaxis, :]  # (K, N, M)
         weighted_diff = xp.sqrt(ru.T)[:, :, xp.newaxis] * diff  # (K, N, M)
         scale_batch = xp.einsum('kni,knj->kij', weighted_diff, weighted_diff)
@@ -61,7 +121,6 @@ def scale_update_calcs(X, ru, loc_, resp_sum, reg_covar, covariance_type='full',
         idx = xp.arange(M)
         scale_batch[:, idx, idx] += reg_covar
         chol_batch = xp.linalg.cholesky(scale_batch)  # (K, M, M)
-        # Transpose back to (M, M, K) for API compat
         scale_ = xp.transpose(scale_batch, (1, 2, 0))
         scale_decomp = xp.transpose(chol_batch, (1, 2, 0))
 
